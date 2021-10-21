bd866ccec3597b923306b47435b259b6b0ae7da4
Ariel <mileguir23@hotmail.com>
2021-10-21 16:13:41 -0300

    A_star working.

--- LOTR/LOTR/Program.cs
+++ LOTR/LOTR/Program.cs
@@ -8,14 +8,14 @@ namespace LOTR {
         static void Main(string[] args) {
             A_star aStar = new A_star(new MatrixSerializer());
             
-            t.Start();
+            //t.Start();
             //Grid_node test = aStar.FromSourceToDestiny(new Grid_node(52, 15), new Grid_node(160, 39));
-            t.Stop();
+            //t.Stop();
             
-            Console.WriteLine($"Elapsed time: {(t.ElapsedMilliseconds / 1000.0)}s");
-            MatrixSerializer.printTracedPathOnMap(test);
+            //Console.WriteLine($"Elapsed time: {(t.ElapsedMilliseconds / 1000.0)}s");
+            //MatrixSerializer.printTracedPathOnMap(test);
             
-            //aStar.ToMountDoomAndBack();
+            aStar.ToMountDoomAndBack();
         }
     }
 }
